9cf505f528ba5cd8b063f3b041a6abaadb04c11b
Nick Pawsey <pawsey18@gmail.com>
2021-07-14 12:09:01 -0300

    pre-heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-  res.send(database.users)
+  res.send("database.users working")
 })
 
 app.post("/signin", (req, res) => {
