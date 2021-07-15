037a08448af5c5e2dc8c5cb7d3fe7e135b318599
Nick Pawsey <pawsey18@gmail.com>
2021-07-15 11:58:04 -0300

    undo change

--- server.js
+++ server.js
@@ -100,10 +100,5 @@ app.put("/image", (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-  //console.log(`app is running on port ${process.env.PORT}`)
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  )
+  console.log(`app is running on port ${process.env.PORT}`)
 })
