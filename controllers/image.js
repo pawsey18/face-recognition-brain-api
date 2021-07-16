9abea91446115903e06210855d92595308e7c1b0
Nick Pawsey <pawsey18@gmail.com>
2021-07-16 09:54:54 -0300

    clean up

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  //free api key, steal it if you want, I guess, If you absolutely feel the need to.
+  apiKey: "859b48a8de624466aad422d95b377828"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall
+};
